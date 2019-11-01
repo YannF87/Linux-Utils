f1862e110aaaf53ca97dc7c5ec5bd9fcc91e25df
Yann <7456641+YannF87@users.noreply.github.com>
2019-11-01 19:27:20 +0100

    PS1 Modif

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 #Github
+#	Installer Git
 #	Cloner le repo Linux-Utils
 #	Créer les clés SSH et enregistrer la clé publique dans un fichier
 #
@@ -10,7 +11,8 @@
 #Installer Hperf
 #
 #Personnaliser Bash :
-#		echo 'export PS1=">>>\t | \u@\H \n>>>\w>\$ :"'>>~/.bash_profile
+		PS1='\[\033[01;35m\]###\[\033[01;36m\] \t \[\033[01;31m\]| \[\033[01;34m\]\u@\h\[\033[01;35m\] ### \n\[\033[01;31m\]>>> \[\033[01;32m\]\w\[\033[01;33m\]>\$:\[\033[00m\]' 
+
 #	>> Création des Alias
 #		alias cls='clear'
 #		alias la='ls -la'
