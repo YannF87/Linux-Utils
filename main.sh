6e12a16e7196b142f4b1505a3a341a4d053bf678
Yann <7456641+YannF87@users.noreply.github.com>
2019-11-01 18:30:37 +0100

    Initial main script

---
+++ main.sh
@@ -0,0 +1,20 @@
+#Github
+#	Cloner le repo Linux-Utils
+#	Créer les clés SSH et enregistrer la clé publique dans un fichier
+#
+#Installer Tree
+#Installer VIM
+#	Setup .VIMRC
+#	Copier les plugins
+#
+#Installer Hperf
+#
+#Personnaliser Bash :
+#		echo 'export PS1=">>>\t | \u@\H \n>>>\w>\$ :"'>>~/.bash_profile
+#	>> Création des Alias
+#		alias cls='clear'
+#		alias la='ls -la'
+#
+#	>>Modifier les Variables d'environement
+#		export EDITOR="vim"
+
